ef9bcff38b3dcaa32547fc43edcd6e7b1f3be808
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-07-18 11:12:57 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -12,12 +12,31 @@ typedef enum {
   TOO_HIGH
 } BreachType;
 
+#define PASSIVE_COOLING_LOWLIMIT     0
+#define PASSIVE_COOLING_HIGHLIMIT    35
+#define MED_ACTIVE_COOLING_LOWLIMIT  36
+#define MED_ACTIVE_COOLING_HIGHLIMIT 40
+#define HI_ACTIVE_COOLING_LOWLIMIT   41
+#define HI_ACTIVE_COOLING_HIGHLIMIT  45
+#define setLowLimit(message) message##_LOWLIMIT
+#define setHighLimit(message) message##_HIGHLIMIT
+
+#define MSG_TOO_LOW      "Hi, the temperature is too low\n"
+#define MSG_TOO_HIGH     "Hi, the temperature is too high\n"
+#define Print(message) MSG_##message 
+
+#define THROW_WARNING(message, recepient)                   \
+({                                                          \
+    printf("To: %s\n", recepient);                          \
+    printf("%s", Print(message));                           \
+})
+
 BreachType inferBreach(double value, double lowerLimit, double upperLimit);
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
 #define checkBreach(currentValue, lowerLimit, upperLimit)   \
 ({                                                          \
-    BreachType checkResult = NORMAL;                               \
+    BreachType checkResult = NORMAL;                        \
     if (currentValue < lowerLimit) {                        \
         checkResult = TOO_LOW;                              \
     } else if(currentValue > upperLimit) {                  \
